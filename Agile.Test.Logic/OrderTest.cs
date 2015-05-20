dfc99ba767ffaab42f7c51dd883146f76bd63d0e
Signum Software <framework@signumsoftware.com>
2015-05-20 19:07:20 +0200

    Removing Test.Logic (ExampleEntities)

--- Agile.Test.Logic/OrderTest.cs
+++ Agile.Test.Logic/OrderTest.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Signum.Engine;
@@ -18,30 +18,5 @@ namespace Agile.Test.Logic
         {
             AgileEnvironment.StartAndInitialize();
         }
-
-        [TestMethod]
-        public void OrderTestExample()
-        {
-            using (AuthLogic.UnsafeUserSession("Normal"))
-            {
-                using (Transaction tr = Transaction.Test()) // Transaction.Test avoids nested ForceNew transactions to be independent
-                {
-                    var john = Database.Query<PersonEntity>().SingleEx(p => p.FirstName == "John");
-
-                    var order = john.ConstructFrom(OrderOperation.CreateOrderFromCustomer);
-
-                    var sonic = Database.Query<ProductEntity>().SingleEx(p=>p.ProductName.Contains("Sonic"));
-
-                    var line = order.AddLine(sonic);
-
-                    order.Execute(OrderOperation.SaveNew);
-
-                    Assert.AreEqual(order.TotalPrice, sonic.UnitPrice);
- 
-
-                    //tr.Commit();
-                }
-            }
-        }//OrderTestExample
     }
 }
